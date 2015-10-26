6d73133222dbadc6c220314e23254c054333db5a
HiranoMasaaki <hirano.masaaki@dena.jp>
2015-10-26 18:29:47 +0900

    Setup chat

--- web/static/js/main/index.jsx
+++ web/static/js/main/index.jsx
@@ -1,15 +1,40 @@
 
 import React from 'react';
+import {Socket} from "../../../../deps/phoenix/web/static/js/phoenix"
 
 const Main = React.createClass({
-  getInitialState: function () {
-    return {
-    };
+  componentDidMount: function () {
+    let socket = new Socket("/socket", {params: {token: window.userToken}})
+    socket.connect()
+
+    // Now that you are connected, you can join channels with a topic:
+    let channel           = socket.channel("rooms:lobby", {})
+    let chatInput         = $("#chat-input")
+    let messagesContainer = $("#messages")
+
+    chatInput.on("keypress", event => {
+      if(event.keyCode === 13){
+        channel.push("new_msg", {body: chatInput.val()})
+        chatInput.val("")
+      }
+    })
+
+    channel.on("new_msg", payload => {
+      messagesContainer.append(`<br/>[${Date()}] ${payload.body}`)
+    })
+
+    channel.join()
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
   },
 
   render: function () {
     return (
       <div className="col-md-10 main-stories row">
+        <div className="chat">
+          <div id="messages"></div>
+          <input id="chat-input" type="text"></input>
+        </div>
       </div>
     );
   }
